084caba01361c389ed26bebecb506b0ebae8f321
Daniel Santana <daniel@everymundo.com>
2015-12-16 18:59:15 -0500

    quiet_logs.py to silence the super verbose spark logger

---
+++ quiet_logs.py
@@ -0,0 +1,4 @@
+def quiet_logs( sc ):
+  logger = sc._jvm.org.apache.log4j
+  logger.LogManager.getLogger("org"). setLevel( logger.Level.ERROR )
+  logger.LogManager.getLogger("akka").setLevel( logger.Level.ERROR )
